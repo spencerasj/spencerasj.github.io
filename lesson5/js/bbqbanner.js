b34f34d6328b8a62efaef7adfe94fb0195bee82b
spencerasj <spe19021@byui.edu>
2020-02-12 12:04:52 -0700

    lots of changes

---
+++ lesson5/js/bbqbanner.js
@@ -0,0 +1,13 @@
+﻿
+
+/*  Processing: see if it is friday and display banner for Saturday BBQ
+ * Output: Display banner on Friday only
+ */
+
+function bbqDay() {
+    let dayOfWeek = new Date().getDay();
+    if (dayOfWeek == 3) {
+        message = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
+    }
+       document.getElementById("bbq").innerHTML = message;
+}
