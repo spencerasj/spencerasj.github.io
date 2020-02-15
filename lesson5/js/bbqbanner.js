221689e8a75976df807c035c29ec6469d6887500
spencerasj <spe19021@byui.edu>
2020-02-14 21:55:01 -0700

    updates

--- lesson5/js/bbqbanner.js
+++ lesson5/js/bbqbanner.js
@@ -6,8 +6,8 @@
 
 function bbqDay() {
     let dayOfWeek = new Date().getDay();
-    if (dayOfWeek == 3) {
+    if (dayOfWeek == 5) {
         message = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
     }
-       document.getElementById("bbq").innerHTML = message;
+       document.getElementById("bbq") = message;
 }
