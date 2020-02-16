39c7236652c9e2861d3c810944f313ac424803b2
spencerasj <spe19021@byui.edu>
2020-02-15 22:52:49 -0700

    beginning my js for windchill

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,12 @@
+function getInput() {
+    let t = parseFloat(document.getElementById('tempFahr').value); 
+    let s = parseFloat(document.getElementById('windSpeed').value);
+    let wc = windChill(t, s) 
+       if(t <= 50 && s >= 3) {
+          document.getElementById('outputDiv').innerHTML = wc;}
+  } 
+  function windChill(tempF, speed) {
+    let f = 35.74 + (.6215 * tempF) - ((35.75) * (speed ** .16)) + ((.4275 * tempF) * (speed ** .16));
+        return f;
+}
+  
