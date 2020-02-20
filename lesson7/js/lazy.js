6dcf7ffda668835f1ebc3d16905d11fc4df71dc8
spencerasj <spe19021@byui.edu>
2020-02-20 16:44:42 -0700

    working on lazy loading

--- lesson7/js/lazy.js
+++ lesson7/js/lazy.js
@@ -1,1 +1,13 @@
-const images = document.querySelectorAll("[data-src]")
+let imagesToLoad =document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");        
+    };
+};
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
+
+
+ 
