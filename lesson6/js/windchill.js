4b116f75caa6c9130cfe7e6aa0ede793f3234245
spencerasj <spe19021@byui.edu>
2020-02-29 18:32:33 -0700

    adding css

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,12 +1,13 @@
-function getInput() {
-    let t = parseFloat(document.getElementById('tempFahr').value); 
-    let s = parseFloat(document.getElementById('windSpeed').value);
-    let wc = windChill(t, s) 
-       if(t <= 50 && s >= 3) {
-          document.getElementById('windchill').innerHTML = wc + "&#8457;";}
-  } 
-  function windChill(tempF, speed) {
-    let f = 35.74 + (.6215 * tempF) - ((35.75) * (speed ** .16)) + ((.4275 * tempF) * (speed ** .16));
-        return f;
-}
+
+    let t = parseFloat(document.getElementById('tempFahr').textContent); 
+    let s = parseFloat(document.getElementById('windSpeed').textContent);
+    
+       if (t <= 50 && s > 3) {
+        let wc = Math.round( 35.74 + (.6215 * tempF) - ((35.75) * (speed ** .16)) + ((.4275 * tempF) * (speed ** .16)));
+      }
+       else {
+         wc= "N/A"
+       }
+          document.getElementById('windchill').innerHTML = wc + "&#8457;"
+  
   
