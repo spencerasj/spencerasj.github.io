ecf7fd5ddbb6a2ec0adb7399fc84d289e1fc03b7
spencerasj <spe19021@byui.edu>
2020-03-03 23:12:23 -0700

    trying to add 3 elements per instructions

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -10,9 +10,13 @@ fetch(requestURL)
     for (let i = 0; i < prophets.length; i++ ) {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
+        let h3 = document.createElement('h3');
+        let h4 = document.createElement('h4');
 
         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
+        h3.textContent = "Date of Birth: " + prophets[i].birthdate;
+        h4.textContent = "Place of Birth: " + prophets[i].birthplace;
+        
         card.appendChild(h2);
 
 document.querySelector('div.cards').appendChild(card);
