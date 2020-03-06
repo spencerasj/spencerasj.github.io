2f8b0cecbed22307c7e4d62eb8e463ef8821fc79
spencerasj <spe19021@byui.edu>
2020-03-05 18:08:56 -0700

    added motto

--- lesson9/js/json.js
+++ lesson9/js/json.js
@@ -10,6 +10,7 @@ fetch(requestURL)
     for (let i = 0; i < towns.length; i++ ) {
         let card = document.createElement('section');
         let name = document.createElement('h3');
+        let motto = document.createElement('h4');
         let yearFounded = document.createElement('p');
         let population = document.createElement('p');
         let rainfall = document.createElement('p');
@@ -17,6 +18,7 @@ fetch(requestURL)
 
         if (towns[i].name = ("Fish Haven" || "Preston" || "Soda Springs"));
             name.textContent = towns[i].name;
+            motto.textContent = towns[i].motto;
             yearFounded.textContent = "Year Founded: " + towns[i].yearFounded;
             population.textContent = "Poulation: " + towns[i].currentPopulation;
             rainfall.textContent = "Annual Rainfall: " + towns[i].averageRainfall;
