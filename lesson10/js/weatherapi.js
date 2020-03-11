f83cfe251c8caa7100d1f9475b02251860b052b1
spencerasj <spe19021@byui.edu>
2020-03-10 21:27:03 -0600

    working on js

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -4,11 +4,13 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject);
 
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
- 
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('icon').setAttribute('alt', desc);
-});
+    for (let i = 0; i < jsObject.list.length; i++ ) {
+        document.getElementById('current-temp').textContent = ((((jsObject.list[i].main.temp) - 273.15) * 1.8 + 32).toFixed(2));
+    }
+    for (let i = 0; i < jsObject.weather.length; i++ ) {
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[i].icon + '.png';  // note the concatenation
+        const desc = jsObject.weather[i].description;  // note how we reference the weather array
+        document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+        document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+        document.getElementById('icon').setAttribute('alt', desc);
+}});
