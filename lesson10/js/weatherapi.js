71f77fdb85cf1746beaed7b858861b2d9ce0d407
spencerasj <spe19021@byui.edu>
2020-03-12 11:31:44 -0600

    trying to fix some things

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=97617174116da9f8f4bdafa9bc37bff2";
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=97617174116da9f8f4bdafa9bc37bff2";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
@@ -6,7 +6,8 @@ fetch(apiURL)
 
     for (let i = 0; i < jsObject.list.length; i++ ) {
         document.getElementById('current-temp').textContent = ((((jsObject.list[i].main.temp) - 273.15) * 1.8 + 32).toFixed(2));
-        
+    }   
+    for (let i = 0; i < jsObject.list.length; i++ ) {    
         const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[i].icon + '.png';  // note the concatenation
         const desc = jsObject.weather[i].description;  // note how we reference the weather array
         document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
