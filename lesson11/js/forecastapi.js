16b6dad248192aa67290883d61b3a85f2d4555f4
spencerasj <spe19021@byui.edu>
2020-03-19 23:03:08 -0600

    trying to make forecast and sum work

--- lesson11/js/forecastapi.js
+++ lesson11/js/forecastapi.js
@@ -11,10 +11,62 @@ fetch(forecastAPI)
                 const f = (day.main.temp);
                 var fahrenheit = f
                 document.getElementById("data" + day_count).textContent = ((fahrenheit - 273.15) * 1.8 + 32).toFixed(0);
-                const imagesrc = 'https://openweathermap.org/img/w/' + day.weather[0].icon + '.png'; 
+                const imagesrc = 'https://openweathermap.org/img/w/' + day.weather[0].icon + '.png';
                 console.log(imagesrc);
-                const desc = day.weather[0].description; 
+                const desc = day.weather[0].description;
                 let idimg = 'temp' + day_count;
+                document.getElementById(idimg).setAttribute('src', imagesrc); // focus on the setAttribute() method
+                document.getElementById(idimg).setAttribute('alt', desc);
+                day_count = day_count + 1;
+            }
+        }
+
+    });
+
+
+const fishforecastAPI = "https://api.openweathermap.org/data/2.5/forecast?zip=83287,us&APPID=97617174116da9f8f4bdafa9bc37bff2";
+fetch(fishforecastAPI)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        var day_count = 1;
+        for (var day of jsObject.list) {
+            if (day.dt_txt.includes("18:00:00")) {
+
+                const f = (day.main.temp);
+                var fahrenheit = f
+                document.getElementById("fishdata" + day_count).textContent = ((fahrenheit - 273.15) * 1.8 + 32).toFixed(0);
+                const imagesrc = 'https://openweathermap.org/img/w/' + day.weather[0].icon + '.png';
+                console.log(imagesrc);
+                const desc = day.weather[0].description;
+                let idimg = 'fishtemp' + day_count;
+                document.getElementById(idimg).setAttribute('src', imagesrc); // focus on the setAttribute() method
+                document.getElementById(idimg).setAttribute('alt', desc);
+                day_count = day_count + 1;
+            }
+        }
+
+    });
+
+
+const ssforecastAPI = "https://api.openweathermap.org/data/2.5/forecast?zip=83276,us&APPID=97617174116da9f8f4bdafa9bc37bff2";
+fetch(ssforecastAPI)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        var day_count = 1;
+        for (var day of jsObject.list) {
+            if (day.dt_txt.includes("18:00:00")) {
+
+                const f = (day.main.temp);
+                var fahrenheit = f
+                document.getElementById("ssdata" + day_count).textContent = ((fahrenheit - 273.15) * 1.8 + 32).toFixed(0);
+                const imagesrc = 'https://openweathermap.org/img/w/' + day.weather[0].icon + '.png';
+                console.log(imagesrc);
+                const desc = day.weather[0].description;
+                let idimg = 'sstemp' + day_count;
                 document.getElementById(idimg).setAttribute('src', imagesrc); // focus on the setAttribute() method
                 document.getElementById(idimg).setAttribute('alt', desc);
                 day_count = day_count + 1;
--- lesson11/js/summary.js
+++ lesson11/js/summary.js
@@ -10,4 +10,30 @@ fetch(apiURL)
         document.getElementById('humidity').textContent = (jsObject.main.humidity);
         document.getElementById('windSpeed').textContent = (jsObject.wind.speed.toFixed(0));
     });
+
+const fishapiURL = "https://api.openweathermap.org/data/2.5/weather?zip=83287,us&APPID=97617174116da9f8f4bdafa9bc37bff2";
+fetch(fishapiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        // document.getElementById('current-temp').textContent = jsObject.main.temp;
+        document.getElementById('fishcurrentdesc').textContent = (jsObject.weather[0].description);
+        document.getElementById('fishtempFahr').textContent = ((((jsObject.main.temp_max) - 273.15) * 1.8 + 32).toFixed(0));
+        document.getElementById('fishhumidity').textContent = (jsObject.main.humidity);
+        document.getElementById('fishwindSpeed').textContent = (jsObject.wind.speed.toFixed(0));
+    });
+
+const ssapiURL = "https://api.openweathermap.org/data/2.5/weather?zip=83276,us&APPID=97617174116da9f8f4bdafa9bc37bff2";
+fetch(ssapiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+
+        // document.getElementById('current-temp').textContent = jsObject.main.temp;
+        document.getElementById('sscurrentdesc').textContent = (jsObject.weather[0].description);
+        document.getElementById('sstempFahr').textContent = ((((jsObject.main.temp_max) - 273.15) * 1.8 + 32).toFixed(0));
+        document.getElementById('sshumidity').textContent = (jsObject.main.humidity);
+        document.getElementById('sswindSpeed').textContent = (jsObject.wind.speed.toFixed(0));
+    });
 windchill();
