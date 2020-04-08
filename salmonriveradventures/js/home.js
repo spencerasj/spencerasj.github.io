574af2c085111e6a53e65f50da04faa8632808f7
spencerasj <spe19021@byui.edu>
2020-04-08 17:14:52 -0600

    windchill fixes

--- salmonriveradventures/js/home.js
+++ salmonriveradventures/js/home.js
@@ -9,12 +9,13 @@ fetch(apiURL)
   .then((jsObject) => {
     console.log(jsObject);
 
-    // document.getElementById('current-temp').textContent = jsObject.main.temp;
-    document.getElementById('currentdesc').textContent = ((((jsObject.main.temp) - 273.15) * 1.8 + 32).toFixed(0));
+    
+    document.getElementById('current').textContent = ((((jsObject.main.temp) - 273.15) * 1.8 + 32).toFixed(0));
     document.getElementById('tempFahr').textContent = ((((jsObject.main.temp_max) - 273.15) * 1.8 + 32).toFixed(0));
     document.getElementById('humidity').textContent = (jsObject.main.humidity);
     document.getElementById('windSpeed').textContent = (jsObject.wind.speed.toFixed(0));
-    windchill(tempFahr, windSpeed);
+    
+windchill(current, windSpeed);
 
   });
   function windchill(t,s) {
