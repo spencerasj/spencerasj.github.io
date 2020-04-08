84a60dc29419df20d540ab522d504f251e57518a
spencerasj <spe19021@byui.edu>
2020-04-08 17:04:21 -0600

    changes to text sizes

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,7 +1,7 @@
 
     let t = parseFloat(document.getElementById('tempFahr').textContent); 
     let s = parseFloat(document.getElementById('windSpeed').textContent);
-    
+    let wc = 0;
        if (t <= 50 && s > 3) {
         let wc = Math.round( 35.74 + (.6215 * tempF) - ((35.75) * (speed ** .16)) + ((.4275 * tempF) * (speed ** .16)));
       }
@@ -10,4 +10,4 @@
        }
           document.getElementById('windchill').innerHTML = wc + "&#8457;"
   
-  
+      
