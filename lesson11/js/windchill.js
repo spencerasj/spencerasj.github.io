d28825631e6ff409f3f33e54d39851e553f197f1
spencerasj <spe19021@byui.edu>
2020-04-07 23:01:14 -0600

    windchill fixes

--- lesson11/js/windchill.js
+++ lesson11/js/windchill.js
@@ -1,7 +1,7 @@
 function windchill(t,s, id) {
   //let t = parseFloat(document.getElementById('tempFahr').textContent);
   //let s = parseFloat(document.getElementById('windSpeed').textContent);
-  let wc;
+  let wc = 0;
   
   if (t <= 50 && s > 3) {
     wc = Math.round(35.74 + .6215 * t - 35.75 * Math.pow(s, .16) + .4275 * t * Math.pow(s, .16));
--- salmonriveradventures/js/home.js
+++ salmonriveradventures/js/home.js
@@ -20,7 +20,7 @@ fetch(apiURL)
   function windchill(t,s) {
     //let t = parseFloat(document.getElementById('tempFahr').textContent);
     //let s = parseFloat(document.getElementById('windSpeed').textContent);
-    let wc;
+    let wc = 0;
     
     if (t <= 50 && s > 3) {
       wc = Math.round(35.74 + .6215 * t - 35.75 * Math.pow(s, .16) + .4275 * t * Math.pow(s, .16));
