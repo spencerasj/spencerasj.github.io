b16c489101385218b3af0a443fab846030308b58
spencerasj <spe19021@byui.edu>
2020-04-09 12:40:52 -0600

    fix summary

--- lesson10/js/summary.js
+++ lesson10/js/summary.js
@@ -4,10 +4,9 @@ fetch(apiURL)
     .then((jsObject) => {
         console.log(jsObject);
 
-        // document.getElementById('current-temp').textContent = jsObject.main.temp;
         document.getElementById('current-temp').textContent =((((jsObject.main.temp) - 273.15) * 1.8 + 32).toFixed(0));
         document.getElementById('tempFahr').textContent = ((((jsObject.main.temp_max) - 273.15) * 1.8 + 32).toFixed(0));
         document.getElementById('humidity').textContent = (jsObject.main.humidity);
         document.getElementById('windSpeed').textContent = (jsObject.wind.speed.toFixed(0));
+        windchill(((((jsObject.main.temp) - 273.15) * 1.8 + 32).toFixed(0)), (jsObject.wind.speed.toFixed(0)));
     });
-windchill(((((jsObject.main.temp) - 273.15) * 1.8 + 32).toFixed(0)), (jsObject.wind.speed.toFixed(0)));
